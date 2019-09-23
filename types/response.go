5a081ba23fb729af327eef65f3ec6aa422b56eae
Tony Stark <ironman0x7b2@protonmail.com>
2019-09-23 15:22:41 +0530

    Added more fields to API logger (#4)

--- types/response.go
+++ types/response.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"net/http"
+)
+
 type Error struct {
 	Code    int         `json:"code,omitempty"`
 	Message string      `json:"message,omitempty"`
@@ -10,4 +14,34 @@ type Response struct {
 	Success bool        `json:"success"`
 	Error   interface{} `json:"error,omitempty"`
 	Result  interface{} `json:"result,omitempty"`
+}
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	Status int
+	Length int
+}
+
+func NewResponseWriter(w http.ResponseWriter) *ResponseWriter {
+	return &ResponseWriter{
+		ResponseWriter: w,
+		Status:         0,
+		Length:         0,
+	}
+}
+
+func (r *ResponseWriter) Header() http.Header {
+	return r.ResponseWriter.Header()
+}
+
+func (r *ResponseWriter) Write(p []byte) (n int, err error) {
+	n, err = r.ResponseWriter.Write(p)
+	r.Length += n
+
+	return n, err
+}
+
+func (r *ResponseWriter) WriteHeader(status int) {
+	r.ResponseWriter.WriteHeader(status)
+	r.Status = status
 }
