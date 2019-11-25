bc39e4043d51a375dea41cf3b400c42f06dbcbba
bitsndbyts <bitsndbyts@protonmail.com>
2019-11-25 15:02:16 +0530

    Added transaction module

---
+++ handlers/tx/routes.go
@@ -0,0 +1,14 @@
+package tx
+
+import (
+	"github.com/gorilla/mux"
+
+	_cli "github.com/ironman0x7b2/client/cli"
+)
+
+func RegisterRoutes(r *mux.Router, cli *_cli.CLI) {
+
+	r.Name("GetTransactionDetails").
+		Methods("GET").Path("/txs/{hash}").
+		HandlerFunc(getTx())
+}
