73920cd38d160c0e6a92a534d91223ccced22619
bitsndbyts <bitsndbyts@protonmail.com>
2020-01-18 13:11:05 +0530

    Fixed recovery account

--- handlers/key/routes.go
+++ handlers/key/routes.go
@@ -10,6 +10,9 @@ func RegisterRoutes(r *mux.Router, cli *_cli.CLI) {
 	r.Name("GetKeys").
 		Methods("GET").Path("/keys").
 		HandlerFunc(getKeysHandler(cli))
+	r.Name("GetKey").
+		Methods("GET").Path("/keys/{address}").
+		HandlerFunc(getKeysWithPrefixHandler(cli))
 	r.Name("AddKey").
 		Methods("POST").Path("/keys").
 		HandlerFunc(addKeyHandler(cli))
